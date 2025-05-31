9fb43c09ddd1267099d32dc76a46ee10492da3c4
Domi-975 <valtiare.25@gmail.com>
2025-05-31 16:08:06 -0400

    tratando de subirlo a github

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-     <Navbar />
-     <Home />
-     <Footer />
-      
-    </div>
-  )
+    <Router basename="/pizzeria-mamma-mia">
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
